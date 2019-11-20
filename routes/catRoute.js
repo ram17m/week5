463c1fa213dac5ae5282e33775274e9fb9c32388
Ram babu Mishra <ramm.metropolia.fi>
2019-11-20 13:25:20 +0200

    git update

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,30 +1,24 @@
 'use strict';
-// catRoute
 const express = require('express');
 const router = express.Router();
+//const multer = require('multer');
+//const upload = multer({dest: 'uploads/'});
 const catController = require('../controllers/catController');
 
-/*router.get('/', (req, res) => {
-    res.send('With this endpoint you can get cats.');
-  });*/
-  router.get('/', catController.cat_list_get);
-  
-  router.get('/:id', (req, res) => {
-    res.send('You requested a cat whose id is ' + req.params.id);
-  });
-  
-  router.post('/', (req, res) => {
-    res.send('With this endpoint you can add cats.');
-  });
-  
-  router.put('/', (req, res) => {
-    res.send('With this endpoint you can edit cats.');
-  });
-  
-  
-  router.delete('/', (req, res) => {
-    res.send('With this endpoint you can delete cats.');
-  });
+router.get('/', catController.cat_list_get);
 
+router.get('/:id', (req, res) => {
+  res.send('You reqested a cat whose id is ' + req.params.id);
+});
 
-  module.exports = router;
+//router.post('/', upload.single('cat'), catController.cat_create_post);
+
+router.put('/', (req, res) => {
+  res.send('With this endpoint you can modify cats.');
+});
+
+router.delete('/', (req, res) => {
+  res.send('With this endpoint you can delete cats.');
+});
+
+module.exports = router;
