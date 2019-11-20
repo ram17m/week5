7316d2486233c17cee5a232cdcd738afaa6cf269
Ram babu Mishra <ramm.metropolia.fi>
2019-11-20 13:37:34 +0200

    using multer to upload file

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,6 +1,4 @@
 'use strict';
-// Controller
-
 const catModel = require('../models/catModel');
 
 const cats = catModel.cats;
@@ -8,7 +6,13 @@ const cats = catModel.cats;
 const cat_list_get = (req, res) => {
   res.json(cats);
 };
-   
+
+const cat_create_post = (req, res) => {
+  res.send('With this endpoint you can add cats.');
+};
+
+
 module.exports = {
   cat_list_get,
+  cat_create_post,
 };
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,8 +1,8 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-//const multer = require('multer');
-//const upload = multer({dest: 'uploads/'});
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const catController = require('../controllers/catController');
 
 router.get('/', catController.cat_list_get);
@@ -11,7 +11,7 @@ router.get('/:id', (req, res) => {
   res.send('You reqested a cat whose id is ' + req.params.id);
 });
 
-//router.post('/', upload.single('cat'), catController.cat_create_post);
+router.post('/', upload.single('cat'), catController.cat_create_post);
 
 router.put('/', (req, res) => {
   res.send('With this endpoint you can modify cats.');
