72a37d7382c85e58b0cb06eeaa3f24255611353d
Ram babu Mishra <ramm.metropolia.fi>
2019-12-04 09:45:51 +0200

    prcess.env fix

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 "use strict";
+
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json()); // for parsing application/json
